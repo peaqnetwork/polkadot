c252575360bdbbe1feac0ed6d6fbb809025b3cb7
tgmichel <telmo@purestake.com>
2022-01-18 09:34:53 +0100

    Update deps

--- scripts/run_benches_for_runtime.sh
+++ scripts/run_benches_for_runtime.sh
@@ -2,7 +2,7 @@
 
 # Runs all benchmarks for all pallets, for a given runtime, provided by $1
 # Should be run on a reference machine to gain accurate benchmarks
-# current reference machine: https://github.com/paritytech/substrate/pull/5848
+# current reference machine: https://github.com/purestake/substrate/pull/5848
 
 runtime="$1"
 standard_args="--release --locked --features=runtime-benchmarks"
